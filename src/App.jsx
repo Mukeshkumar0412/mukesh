c37736405871bc26a0cacdf144ea91c9f89e8092
Mukeshkumar0412 <mukeshsnm0420@gmail.com>
2024-09-26 20:35:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Header from './comp/Header'
+import Hero from './comp/Hero'
+import Banner from './comp/Banner'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Hero/>
+      <Banner/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/comp/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      <header>
+        <nav>
+          <div class="nav-links">
+            <a href="#home">home</a>
+            <a href="#band">band</a>
+            <a href="#tour">tour</a>
+            <a href="#contract">contact</a>
+            <a href="">more</a>
+
+          </div>
+          <div>
+            <i class="fa fa-search"></i>
+          </div>
+        </nav>
+      </header>
+    </div>
+  )
+}
